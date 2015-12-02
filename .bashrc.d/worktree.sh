ab47001a70ed7dcc52e77eb08882a171e1026c14
Edward Samson <edward@samson.ph>
2015-12-02 15:36:23 +0800

    Add helper function to switch between git worktrees

---
+++ .bashrc.d/worktree.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+#
+# Git worktree switcher
+#
+# Usage:
+#     Source this in your .bashrc and call `worktree` in your working
+#     directory. Calling with no arguments lists existing worktrees if any.
+#     
+#     `worktree <name>` changes current directory to named worktree.
+#     `worktree --main` changes current directory to main working copy.
+#
+# TODO:
+#     * Work from within any subdirectory of a git working copy.
+
+__worktree_cd() {
+    MAIN_DIR=$1
+    WORK_TREE=$2
+
+    if [ "--main" == "$WORK_TREE" ] || [ "-m" == "$WORK_TREE" ]; then
+        cd $MAIN_DIR
+        return
+    fi
+
+    if [ ! -d "$MAIN_DIR/.git/worktrees/$WORK_TREE" ]; then
+        echo "No such worktree '$WORK_TREE'"
+        return 1
+    fi
+
+    gitdir=$(cat $MAIN_DIR/.git/worktrees/$WORK_TREE/gitdir)
+    dir=$(dirname $gitdir)
+    cd $dir
+}
+
+__worktree_ls() {
+    MAIN_DIR=$1
+    echo "Worktrees:"
+
+    for worktree in $MAIN_DIR/.git/worktrees/* ; do
+        name=$(basename $worktree)
+        dir=$(dirname $(cat $worktree/gitdir))
+        echo $'\t'"$name "$'\t'"- $dir"
+    done
+    echo '`worktree <name>` switches to named worktree'
+}
+
+__worktree_in_maindir() {
+    if [ ! -d ".git/worktrees" ]; then
+        echo "No worktrees"
+        return
+    fi
+
+    if [ -z "$1" ]; then
+        __worktree_ls $PWD
+    else
+        __worktree_cd $PWD $1
+    fi
+}
+
+__worktree_in_workdir() {
+    MAIN_DIR=$(cat .git | sed 's/gitdir: //' | sed 's/\.git.*//')
+
+    if [ -z "$1" ]; then
+        echo "In worktree $PWD"
+        echo "       from $MAIN_DIR"
+        echo '`worktree --main` switches to main dir'
+        echo
+        __worktree_ls $MAIN_DIR
+    else
+        __worktree_cd $MAIN_DIR $1
+    fi
+}
+ 
+worktree() {
+    if [ -d ".git" ]; then
+        __worktree_in_maindir $*
+    elif [ -f ".git" ]; then
+        __worktree_in_workdir $*
+    else
+        echo "Not in a git workspace"
+        return 1
+    fi
+}
