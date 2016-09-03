aaa8809751d2eb6fb62baf1f9fac4744fc5f02c5
Edward Samson <edward@samson.ph>
2016-09-03 14:13:51 +0800

    Optional $HOME/.java_home_opts to control JAVA_HOME for Mac
    By default, /usr/libexec/java_home will select the latest Java version.
    This works fine in most cases but in some places (e.g., at work) I need
    to use an older Java.
    
    To control this, create a $HOME/.java_home_opts file that sets the
    java_home_opts variable.

--- .bash_profile.d/Darwin/java.sh
+++ .bash_profile.d/Darwin/java.sh
@@ -1,2 +1,7 @@
-export JAVA_HOME="$(/usr/libexec/java_home -v 1.7)"
+java_home_opts=""
+opts_file=$HOME/.java_home_opts
+if [ -r $opts_file ]; then
+    source $opts_file
+fi
+export JAVA_HOME="$(/usr/libexec/java_home $java_home_opts)"
 pathmunge $JAVA_HOME/bin
