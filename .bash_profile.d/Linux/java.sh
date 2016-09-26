50b9efab4e61484a3707b2d638291d39e0ac95ac
Edward Samson <edward@samson.ph>
2016-09-26 20:39:13 +0800

    On Linux, use packaged JDK if provided

--- .bash_profile.d/Linux/java.sh
+++ .bash_profile.d/Linux/java.sh
@@ -2,5 +2,8 @@ java_home=$HOME/.local/opt/jdk
 if [ -f $java_home/bin/javac ]; then
     export JAVA_HOME=$java_home
     pathmunge $JAVA_HOME/bin
+elif [ -f /usr/bin/javac ]; then
+    export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+    pathmunge $JAVA_HOME/bin
 fi
 unset java_home
