258f89f7c80c90bae55f279fd5462ba6b4490e67
Edward Samson <edward@samson.ph>
2017-03-10 10:46:45 +0800

    Backup bash history every hour

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,6 +1,10 @@
+# git prompt
 source ~/.local/lib/git-prompt.sh
 
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWUNTRACKEDFILES=1
 GIT_PS1_SHOWCOLORHINTS=1
 PROMPT_COMMAND='__git_ps1 "[\[\033[36m\]\u\[\033[m\]@\h \[\033[32m\]\W\[\033[m\]]" "\\\$ " "[%s]"'
+
+# immediately append to bash history
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
