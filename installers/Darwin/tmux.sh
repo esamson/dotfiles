784af67f7372fe5ab59a1024fc14eb168250ec38
Edward Samson <edward@samson.ph>
2017-08-10 08:40:34 +0800

    Homebrew no longer provides reattach-user-namespace wrappers
    See: https://github.com/Homebrew/homebrew-core/pull/10668

--- installers/Darwin/tmux.sh
+++ installers/Darwin/tmux.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
 brew install tmux
-
-# Make pasteboard work for tmux in macOS Sierra
-# https://github.com/tmux/tmux/issues/543
-brew install reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste
+brew install reattach-to-user-namespace
