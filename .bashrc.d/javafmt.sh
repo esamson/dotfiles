ed98d07596af8dc1a3c954a5add517fbb78a9101
Edward Samson <edward@samson.ph>
2017-10-25 12:21:24 +0800

    New javafmt alias to process an entire directory tree

--- .bashrc.d/javafmt.sh
+++ .bashrc.d/javafmt.sh
@@ -1,1 +1,8 @@
-alias javafmt="google-java-format --aosp"
+# https://github.com/google/google-java-format/pull/50#issuecomment-223630180
+javafmt() {
+    DIR="$1"
+    if [ -z "$DIR" ]; then
+        DIR="."
+    fi
+    find "$DIR" -type f -name "*.java" | xargs google-java-format --replace --aosp
+}
