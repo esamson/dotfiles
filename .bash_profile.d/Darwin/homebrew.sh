1c3869054730181c1498acea45ed43567521d9bb
Edward Samson <edward@samson.ph>
2019-05-11 08:45:24 +0800

    Export HOMEBREW_GITHUB_API_TOKEN from file

--- .bash_profile.d/Darwin/homebrew.sh
+++ .bash_profile.d/Darwin/homebrew.sh
@@ -5,4 +5,9 @@ if hash brew 2>/dev/null; then
         export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
         source "$bash_completion"
     fi
+
+    homebrew_github_api_token="$HOME/.homebrew_github_api_token"
+    if [ -r "$homebrew_github_api_token" ]; then
+        export HOMEBREW_GITHUB_API_TOKEN="$(cat $homebrew_github_api_token)"
+    fi
 fi
