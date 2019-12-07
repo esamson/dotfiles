8e3b3cf21eb49f6224881e0ccd181b61d6998580
Edward Samson <edward@samson.ph>
2019-12-07 18:07:12 +0800

    Create symbolic links to Kafka scripts instead of aliases
    Aliases are less useful since they can't be called from within a bash
    script.

--- .bash_profile.d/kafka.sh
+++ .bash_profile.d/kafka.sh
@@ -1,14 +1,18 @@
 kafka_home=$HOME/.local/opt/kafka
 if [ -f $kafka_home/bin/kafka-topics.sh ]; then
-    # We could also just
-    #     pathmunge $kafka_home/bin after
-    # But we'll alias to names without `.sh` suffix so usage is the same as
-    # when Kafka is installed on macOS using Homebrew.
-    for script_sh in $kafka_home/bin/*.sh; do
-        [ -e "$script_sh" ] || continue
-        file=${script_sh##*/}
-        name=${file%.*}
-        alias $name=$script_sh
-    done
+    pathmunge $kafka_home/bin after
+
+    if [ ! -f $kafka_home/bin/kafka-topics ]; then
+        # We'll link to names without `.sh` suffix so usage is the same as
+        # when Kafka is installed on macOS using Homebrew.
+        for script_sh in $kafka_home/bin/*.sh; do
+            [ -e "$script_sh" ] || continue
+            file=${script_sh##*/}
+            name=${file%.*}
+            if [ ! -f $kafka_home/bin/$name ]; then
+                ln -s $script_sh $kafka_home/bin/$name
+            fi
+        done
+    fi
 fi
 unset kafka_home
