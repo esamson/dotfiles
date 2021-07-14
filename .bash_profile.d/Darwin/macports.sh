7980a55bf36f6ba31a8964239ef71f31de1edd64
Edward Samson <edward@samson.ph>
2021-07-14 08:53:51 +0800

    macports bash completion

--- .bash_profile.d/Darwin/macports.sh
+++ .bash_profile.d/Darwin/macports.sh
@@ -1,3 +1,12 @@
 if [ -x /opt/local/bin/port ]; then
     pathmunge /opt/local/bin
+
+    if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
+        . /opt/local/etc/profile.d/bash_completion.sh
+
+        # There might be a better way to do this but this works for now.
+        if [ -f /opt/local/share/git/contrib/completion/git-completion.bash ]; then
+            . /opt/local/share/git/contrib/completion/git-completion.bash
+        fi
+    fi
 fi
