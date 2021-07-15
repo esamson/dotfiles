6b790614476c76ec6c6d5cd0133735ca10067a85
Edward Samson <edward@samson.ph>
2021-07-15 15:41:40 +0800

    macports: add /opt/local/sbin to PATH

--- .bash_profile.d/Darwin/macports.sh
+++ .bash_profile.d/Darwin/macports.sh
@@ -1,5 +1,6 @@
 if [ -x /opt/local/bin/port ]; then
     pathmunge /opt/local/bin
+    pathmunge /opt/local/sbin
 
     if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
         . /opt/local/etc/profile.d/bash_completion.sh
