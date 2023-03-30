16a189a5e3bc8c0e1ed6fad2ad1e35faa7e1c6ad
Edward Samson <edward@samson.ph>
2023-03-30 10:41:20 +0800

    Also use fzf installed through Homebrew

--- .bashrc.d/fzf.sh
+++ .bashrc.d/fzf.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-fzf_bindings=/usr/share/fzf/shell/key-bindings.bash
-if [ -f "$fzf_bindings" ] ; then
-    source $fzf_bindings
+brew_fzf="$HOME/.fzf.bash"
+fedora_fzf=/usr/share/fzf/shell/key-bindings.bash
+
+if [ -f "$brew_fzf" ]; then
+    fzf_bash=$brew_fzf
+else
+    fzf_bash=$fedora_fzf
+fi
+
+if [ -f "$fzf_bash" ] ; then
+    source $fzf_bash
 fi
