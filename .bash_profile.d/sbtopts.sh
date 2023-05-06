38ec35c008ead3be6ec61f920f567751c2615a32
Edward Samson <edward@samson.ph>
2023-05-06 21:47:12 +0800

    Use sbtopts in dotfiles
    No need to move sbtopts to /etc.

--- .bash_profile.d/sbtopts.sh
+++ .bash_profile.d/sbtopts.sh
@@ -1,3 +1,4 @@
-if [ -f "$HOME/.sbt/repositories" ]; then
-  export SBT_OPTS="-Dsbt.override.build.repos=true"
+sbt_opts_file="$HOME/.local/etc/sbtopts"
+if [ -f "$sbt_opts_file" ]; then
+    export SBT_ETC_FILE="$sbt_opts_file"
 fi
