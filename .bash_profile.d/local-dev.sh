77fb903268be65d2c5181ffdf01b490ca6648dd6
Edward Samson <edward@samson.ph>
2023-12-22 14:46:18 +0800

    Add local-dev scripts to PATH
    For things I don't want to show up in public dotfiles.

--- .bash_profile.d/local-dev.sh
+++ .bash_profile.d/local-dev.sh
@@ -1,5 +1,5 @@
-local_dev="$HOME/Documents/local-dev/bash_profile.d"
-if [ -d "$local_dev" ]; then
+local_dev="$HOME/Documents/local-dev"
+if [ -d "$local_dev/bash_profile.d" ]; then
     for script in $local_dev/*.sh ; do
         if [ -r $script ] ; then
             . $script
@@ -7,4 +7,9 @@ if [ -d "$local_dev" ]; then
     done
     unset script
 fi
+
+if [ -d "$local_dev/bin" ]; then
+    pathmunge "$local_dev/bin" after
+fi
+
 unset local_dev
