2e3568aa016557c0e8585fd62dd4300c158ecbf9
Edward Samson <edward@samson.ph>
2025-09-11 09:43:56 +0800

    Update macos tmux config
    reattach-to-user-namespace is no longer necessary for recent tmux
    versions.
    
    Resetting PATH does not play well with nix-darwin managed tmux.

--- .bash_profile.d/Darwin/00-tmux-path.sh
+++
@@ -1,8 +0,0 @@
-# Prevent default MacOS /etc/profile from messing with PATH order in tmux.
-#
-# see: https://superuser.com/a/583502
-
-if [ -n "$TMUX" ] && [ -f /etc/profile ]; then
-    PATH=""
-    source /etc/profile
-fi
